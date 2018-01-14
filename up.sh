2009ac4ecbcc2291a024ab88e2a8eff372f43b4f
dom <dominic.dipasquale@gmail.com>
2018-01-14 15:10:07 +1000

    Add file for updating container

---
+++ up.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# Step one find and kill the container currently running
+docker stop $(docker ps | grep "domsland" | awk '{print $1;}')
+# Rebuild it
+docker build -t domsland .
+# Finally bring it back up
+docker run -d -p 3045:3045 

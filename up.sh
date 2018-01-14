e21fabaf67e0469255d876e5f6f72aeabef490d0
dom <dominic.dipasquale@gmail.com>
2018-01-14 15:11:24 +1000

    fix up.sh, now starts the correct container

--- up.sh
+++ up.sh
@@ -5,4 +5,4 @@ docker stop $(docker ps | grep "domsland" | awk '{print $1;}')
 # Rebuild it
 docker build -t domsland .
 # Finally bring it back up
-docker run -d -p 3045:3045 
+docker run -d -p 3045:3045 domsland

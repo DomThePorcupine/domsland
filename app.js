03004ebeff1494078fd00258e042185d6ae3cd7b
dom <dominic.dipasquale@gmail.com>
2018-09-11 21:26:38 -0400

    Fix security issues

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 var express = require('express')
-var serveStatic = require('serve-static')
 
 var app = express()
 
-app.use(serveStatic('pub', {'index': ['index.html', 'index.htm']}))
+app.use(express.static('pub'));
 
 app.use(function(req, res) {
   res.sendFile(__dirname + '/pub/404.html')
